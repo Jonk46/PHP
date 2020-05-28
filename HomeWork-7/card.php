fe3e4e288ad9be997b534ec3e087e6ccd0201eb6
Alex <jonk6233@gmail.com>
2020-05-28 21:30:50 +0300

    buyer

--- HomeWork-7/card.php
+++ HomeWork-7/card.php
@@ -1,58 +1,55 @@
 <?php
 session_start();
 
-include_once "products.php";
+require_once 'products.php';
 
 if ($_GET['action'] === 'add') {
+    
     if (!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = [
-            'products' => [],
+            'products' => [
+                //'100' => '3'
+            ],
         ];
     }
- if (!in_array($_GET['products_id'], array_keys($_SESSION['cart']['products']))) {
-    $_SESSION['cart']['products'][$_GET['product_id']] = 1;
- } else $_SESSION['cart']['products'][$_GET['product_id']] ++;
- header('Location: card.php?action=list');
+    
+    $ids = array_keys($_SESSION['cart']['products']);
+    if (in_array($_GET['product_id'], $ids)) {
+        $_SESSION['cart']['products'][$_GET['product_id']]++;
+    } else {
+        $_SESSION['cart']['products'][$_GET['product_id']] = 1;
+    }
+
+    header('Location: card.php?action=list');
 }
 
-if($_GET['action'] === 'list') {
-    if(count($_SESSION['cart']['products']) > 0 ) {
+
+if ($_GET['action'] === 'list') {
+
+    if (count ($_SESSION['cart']['products']) > 0) {
         $cardItems = [];
-        foreach($_SESSION['cart']['products'] as $product_id => $product_value) {
+        foreach($_SESSION['cart']['products'] as $product_id => $qty) {
             $cardItems[] = (object)[
                 'id' => $product_id,
-                'product_value' => $product_value,
-                'info' => (object)$products[$product_id]
+                'qty' => $qty,
+                'info' => (object)$products[$product_id],
             ];
-         }       }
-    }
-
-    $_SESSION['list'] = $cardItems;
-    
-if (isset($_POST['btn'])) {
-    foreach($_POST['qty']['qty'] as $key => $value) {
-        if($_POST['qty']['qty'][$key] < 0) {
-            $_POST['qty']['qty'][$key] < 1;
         }
+        
+        require_once 'views/card.view.php';
+        exit;
+    } else {
+        echo "Your shoping cart is empty...";
     }
-    $new_values = array_combine(
-        $_POST['qty']['id'],
-        $_POST['qty']['qty']
+}
+
+if (isset($_POST['btn'])) {
+    echo "<pre>";
+    print_r($_POST);
+    var_dump(
+        array_combine(
+            $_POST['qty']['id'],
+            $_POST['qty']['qty']
+        )
     );
-    foreach($new_values as $key => $value) {
-        $_SESSION['cart']['products'][$key] = $value;
-    }
-    header('Location: card.php?action = list');
 }
-
-if($_GET['action'] === 'remove') {
-    unset($_SESSION['cart']['products'][$_GET['product_idrem']]); // глянути що тут мало бути
-    header('Location: card.php?action=list');
-}
-
-if($_GET['action'] === 'remove_all') {
-    ($_SESSION['cart']['products'] = [];
-    header('Location: card.php?action=list');
-}
-
-include_once "card.view.php";
--- HomeWork-7/index.php
+++ HomeWork-7/index.php
@@ -1,14 +1,82 @@
 <?php 
+    session_start();
 
-$myecho = function () 
-{
- if (func_num_args() > 0) {
- $str = "";
- foreach (func_get_args() as $value) {
- $str .= "{$value}<br>"; 
- }
- echo $str;
- }
-};
-$myecho("dfdfd",'sdfsdf');
-exit;
+    require_once 'products.php';
+
+    //session_destroy();
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Pricing example · Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="assets/css/pricing.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Woman Shoes</a>
+            <a class="p-2 text-dark" href="#">Man Shoes</a>
+            <a class="p-2 text-dark" href="#">Children</a>
+        </nav>
+    </div>
+
+    <div class="container">
+        <ul>
+            <?php foreach ($products as $product_id => $product) :?>
+                <li class="mb-3">
+                    <strong><?= $product['manufacturer'] ?></strong> | <?= $product['name'] ?> 
+                    <a class="btn btn-success btn-sm" href="card.php?action=add&product_id=<?= $product_id ?>">
+                        Add to card
+                    </a>
+                </li>
+            <?php endforeach?>
+        </ul>
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <div class="row">
+            <div class="col-12 col-md">
+                <img class="mb-2" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+                <small class="d-block mb-3 text-muted">&copy; 2017-2019</small>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Features</h5>
+                <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Cool stuff</a></li>
+                <li><a class="text-muted" href="#">Random feature</a></li>
+                <li><a class="text-muted" href="#">Team feature</a></li>
+                <li><a class="text-muted" href="#">Stuff for developers</a></li>
+                <li><a class="text-muted" href="#">Another one</a></li>
+                <li><a class="text-muted" href="#">Last time</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Resources</h5>
+                <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Resource</a></li>
+                <li><a class="text-muted" href="#">Resource name</a></li>
+                <li><a class="text-muted" href="#">Another resource</a></li>
+                <li><a class="text-muted" href="#">Final resource</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>About</h5>
+                <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Team</a></li>
+                <li><a class="text-muted" href="#">Locations</a></li>
+                <li><a class="text-muted" href="#">Privacy</a></li>
+                <li><a class="text-muted" href="#">Terms</a></li>
+                </ul>
+            </div>
+            </div>
+        </footer>
+
+    </div>
+
+</body>
+</html>
